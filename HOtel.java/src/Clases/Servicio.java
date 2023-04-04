61132a4d5718d9c4ea4ac92cc8f9d2a12e8748b2
Alejandro-Mariscal <a.mariscal@uniandes.edu.co>
2023-04-03 19:10:12 -0500

    Faltaba servicio

--- HOtel.java/src/Clases/Servicio.java
+++ HOtel.java/src/Clases/Servicio.java
@@ -1,5 +1,11 @@
 package Clases;
+import java.util.ArrayList;
+
 public class Servicio {
+
+    //atributos clases//
+    private ArrayList<Otro> otros;
+    private Restaurante restaurante;
 
     //atributos//
     private double precioConsumo;
